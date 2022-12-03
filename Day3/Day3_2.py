4d81946c12f5337639fd94407ea0d2e9be19d7e9
lostmycookies <94399147+lostmycookies@users.noreply.github.com>
2022-12-02 21:58:20 -0800

    Clutter removed

--- Day3/Day3_2.py
+++ Day3/Day3_2.py
@@ -5,17 +5,12 @@ Created on Fri Dec  2 21:41:35 2022
 @author: lostmycookies
 """
 
-
 with open('input.txt') as f:
-    lines = f.read().splitlines()  
-
-result = ''    
-
-string1 = lines[0]
-string2 = lines[1]
-string3 = lines[2]
+    lines = f.read().splitlines()    
 
 common = ''.join(set(string1).intersection(string2).intersection(string3))
+
+result = ''
 
 for i in range(len(lines))[0:len(lines):3]:
     string1 = lines[i]
