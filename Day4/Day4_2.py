042bcb6c0ca3d493ac8e06e0a9e4f3a78138aeb7
lostmycookies <polzin.benjamin@googlemail.com>
2022-12-03 23:02:10 -0800

    Second part of the day 4 coding challenge.

---
+++ Day4/Day4_2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  3 20:45:22 2022
+
+@author: lostmycookies
+"""
+
+
+import re
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+counter = 0
+    
+for i in range(len(lines)):
+    # Get the sections:
+    elfpair = re.split('-|,',lines[i])
+    elfpair = [int(patch) for patch in elfpair]
+    
+    set1 = set(range(elfpair[0],elfpair[1]+1))
+    set2 = set(range(elfpair[2],elfpair[3]+1))
+    
+    if set1.intersection(set2) != set():
+        counter += 1
+
+print("The final result is %d." % (counter))
