4fa5688e4e5516ef1058ecf34d5b4287dc428539
lostmycookies <polzin.benjamin@googlemail.com>
2022-12-05 11:02:53 -0800

    Added a function wrapping per Thuan's request.

--- Day6/Day6_2.py
+++ Day6/Day6_2.py
@@ -12,11 +12,15 @@ packetmarker = []
 
 messagelength = 14
 
-for i in range(len(inputstring))[0:(len(inputstring)-messagelength-1)]:
-    checkset = set()
-    for j in range(0,messagelength):
-        checkset.add(inputstring[i+j])
-    if len(checkset) == 14:
-        packetmarker.append(i+14)
+def find_marker(inputstring,messagelength):
+    for i in range(len(inputstring))[0:(len(inputstring)-messagelength-1)]:
+        checkset = set()
+        for j in range(0,messagelength):
+            checkset.add(inputstring[i+j])
+        if len(checkset) == messagelength:
+            packetmarker.append(i+messagelength)
+    print('\nThe start of the message marker is at position: %s' % (packetmarker[0]))
+    
+# Call the function:
 
-print('\nThe start of the message marker is at position: %s' % (packetmarker[0]))
+find_marker(inputstring, messagelength)
